9be53411737731a50618a91a535ec7288c9fa1d6
epicgamer <redditgang4life@yandex.ru>
2023-06-18 20:21:58 +0300

    Add time measurement

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <cassert>
+#include <chrono>
 #include <fstream>
 #include <filesystem>
 #include <unordered_map>
@@ -34,17 +35,29 @@ void TestCVRP() {
                 }
             }
 
+            std::cout << std::endl;
             std::cout << "Dataset: " << name_instance_data.begin()->second.dataset_name << std::endl; 
+            float savings_total_time_ms = 0;
             for (const auto& p : name_instance_data) {
                 const auto& name = p.first;
                 const auto& bks = name_bks[name];
+
+                auto begin = std::chrono::high_resolution_clock::now();
                 auto solution = CVRP::SolveSavings(p.second);
+                auto end = std::chrono::high_resolution_clock::now();
+
+                float ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+                savings_total_time_ms += ms;
+
                 float cost = solution.total_cost;
                 assert(CVRP::Verify(p.second, solution));
                 std::cout << name << std::endl;
-                std::cout << "  Savings algorithm: " << cost << ", " << 100 * cost / bks << "% of best known solution" << std::endl;
+                std::cout << "  Savings algorithm: " << cost << std::endl;
+                std::cout << "    " << 100 * cost / bks << "% of best known solution" << std::endl;
+                std::cout << "    Time: " << ms << " ms" << std::endl;
                 std::cout << "  Best known solution: " << bks << std::endl;
             }
+            std::cout << "Total savings algorithm time: " << savings_total_time_ms << " ms" << std::endl;
         }
     }
 }
