823975acfc00fa40547b1210d6e8ad110c08e41a
epicgamer <110288373+pl553@users.noreply.github.com>
2023-06-20 01:36:38 +0300

    Decompose main a bit

--- README.md
+++ README.md
@@ -1,41 +1,41 @@
-## Dataset: Golden et al. 1998
-| Instance | Best known solution | Savings algorithm |
-| --- | --- | --- |
-| Golden_17 | 707.76 | 802.346 <br> 113.364% of BKS <br> 29 ms |
-| Golden_19 | 1365.6 | 1497.41 <br> 109.652% of BKS <br> 70 ms |
-| Golden_03 | 11036.2 | 11987.5 <br> 108.62% of BKS <br> 88 ms |
-| Golden_09 | 579.71 | 628.757 <br> 108.461% of BKS <br> 34 ms |
-| Golden_20 | 1818.32 | 1979.95 <br> 108.889% of BKS <br> 95 ms |
-| Golden_05 | 6460.98 | 6881.33 <br> 106.506% of BKS <br> 19 ms |
-| Golden_07 | 10102.7 | 11577.4 <br> 114.597% of BKS <br> 69 ms |
-| Golden_13 | 857.19 | 971.42 <br> 113.326% of BKS <br> 31 ms |
-| Golden_06 | 8404.26 | 9051.31 <br> 107.699% of BKS <br> 41 ms |
-| Golden_14 | 1080.55 | 1213.27 <br> 112.282% of BKS <br> 52 ms |
-| Golden_01 | 5623.47 | 5599.47 <br> 99.5732% of BKS <br> 29 ms |
-| Golden_04 | 13592.9 | 15817.3 <br> 116.365% of BKS <br> 127 ms |
-| Golden_18 | 995.13 | 1117.77 <br> 112.324% of BKS <br> 46 ms |
-| Golden_10 | 736.26 | 789.589 <br> 107.243% of BKS <br> 64 ms |
-| Golden_15 | 1337.92 | 1511.77 <br> 112.994% of BKS <br> 82 ms |
-| Golden_16 | 1612.5 | 1811.88 <br> 112.365% of BKS <br> 122 ms |
-| Golden_02 | 8404.61 | 8549.04 <br> 101.718% of BKS <br> 54 ms |
-| Golden_11 | 912.84 | 981.716 <br> 107.545% of BKS <br> 91 ms |
-| Golden_12 | 1102.69 | 1187.41 <br> 107.683% of BKS <br> 135 ms |
-| Golden_08 | 11635.3 | 12676 <br> 108.944% of BKS <br> 114 ms |
-|  |  | Total time: 1392ms <br> Average ratio: 109.507% |
 ## Dataset: Christofides et al. 1979
 | Instance | Best known solution | Savings algorithm |
 | --- | --- | --- |
-| CMT05 | 1291.29 | 1575.54 <br> 122.013% of BKS <br> 23 ms |
-| CMT01 | 524.61 | 602.345 <br> 114.818% of BKS <br> 1 ms |
-| CMT07 | 909.68 | 1062.78 <br> 116.83% of BKS <br> 3 ms |
-| CMT14 | 866.37 | 896.833 <br> 103.516% of BKS <br> 4 ms |
-| CMT09 | 1162.55 | 1216.8 <br> 104.667% of BKS <br> 12 ms |
-| CMT11 | 1042.11 | 1223.71 <br> 117.427% of BKS <br> 8 ms |
-| CMT06 | 555.43 | 602.345 <br> 108.447% of BKS <br> 1 ms |
-| CMT10 | 1395.85 | 1575.54 <br> 112.873% of BKS <br> 23 ms |
-| CMT02 | 835.26 | 1062.78 <br> 127.239% of BKS <br> 3 ms |
-| CMT08 | 865.94 | 972.099 <br> 112.259% of BKS <br> 5 ms |
-| CMT12 | 819.56 | 896.833 <br> 109.429% of BKS <br> 4 ms |
-| CMT03 | 826.14 | 972.099 <br> 117.668% of BKS <br> 5 ms |
-| CMT04 | 1028.42 | 1216.8 <br> 118.318% of BKS <br> 11 ms |
-|  |  | Total time: 103ms <br> Average ratio: 114.269% |
+| CMT01 | 524.61 | 602.35 <br> 14.82% over BKS <br> 0.00 ms |
+| CMT02 | 835.26 | 1062.78 <br> 27.24% over BKS <br> 2.00 ms |
+| CMT10 | 1395.85 | 1575.54 <br> 12.87% over BKS <br> 16.00 ms |
+| CMT07 | 909.68 | 1062.78 <br> 16.83% over BKS <br> 2.00 ms |
+| CMT06 | 555.43 | 602.35 <br> 8.45% over BKS <br> 0.00 ms |
+| CMT12 | 819.56 | 896.83 <br> 9.43% over BKS <br> 3.00 ms |
+| CMT04 | 1028.42 | 1216.80 <br> 18.32% over BKS <br> 8.00 ms |
+| CMT03 | 826.14 | 972.10 <br> 17.67% over BKS <br> 3.00 ms |
+| CMT11 | 1042.11 | 1223.71 <br> 17.43% over BKS <br> 5.00 ms |
+| CMT09 | 1162.55 | 1216.80 <br> 4.67% over BKS <br> 8.00 ms |
+| CMT14 | 866.37 | 896.83 <br> 3.52% over BKS <br> 3.00 ms |
+| CMT08 | 865.94 | 972.10 <br> 12.26% over BKS <br> 3.00 ms |
+| CMT05 | 1291.29 | 1575.54 <br> 22.01% over BKS <br> 17.00 ms |
+|  |  | Total time: 70.00ms <br> Average % over BKS: 14.27% |
+## Dataset: Golden et al. 1998
+| Instance | Best known solution | Savings algorithm |
+| --- | --- | --- |
+| Golden_15 | 1337.92 | 1511.77 <br> 12.99% over BKS <br> 65.00 ms |
+| Golden_04 | 13592.88 | 15817.32 <br> 16.36% over BKS <br> 99.00 ms |
+| Golden_02 | 8404.61 | 8549.04 <br> 1.72% over BKS <br> 41.00 ms |
+| Golden_18 | 995.13 | 1117.77 <br> 12.32% over BKS <br> 36.00 ms |
+| Golden_12 | 1102.69 | 1187.41 <br> 7.68% over BKS <br> 103.00 ms |
+| Golden_16 | 1612.50 | 1811.88 <br> 12.36% over BKS <br> 104.00 ms |
+| Golden_13 | 857.19 | 971.42 <br> 13.33% over BKS <br> 24.00 ms |
+| Golden_09 | 579.71 | 628.76 <br> 8.46% over BKS <br> 27.00 ms |
+| Golden_19 | 1365.60 | 1497.41 <br> 9.65% over BKS <br> 53.00 ms |
+| Golden_20 | 1818.32 | 1979.95 <br> 8.89% over BKS <br> 74.00 ms |
+| Golden_03 | 11036.22 | 11987.51 <br> 8.62% over BKS <br> 67.00 ms |
+| Golden_06 | 8404.26 | 9051.31 <br> 7.70% over BKS <br> 31.00 ms |
+| Golden_07 | 10102.68 | 11577.38 <br> 14.60% over BKS <br> 53.00 ms |
+| Golden_14 | 1080.55 | 1213.27 <br> 12.28% over BKS <br> 39.00 ms |
+| Golden_08 | 11635.34 | 12676.00 <br> 8.94% over BKS <br> 81.00 ms |
+| Golden_17 | 707.76 | 802.35 <br> 13.36% over BKS <br> 22.00 ms |
+| Golden_10 | 736.26 | 789.59 <br> 7.24% over BKS <br> 43.00 ms |
+| Golden_11 | 912.84 | 981.72 <br> 7.55% over BKS <br> 70.00 ms |
+| Golden_05 | 6460.98 | 6881.33 <br> 6.51% over BKS <br> 15.00 ms |
+| Golden_01 | 5623.47 | 5599.47 <br> -0.43% over BKS <br> 22.00 ms |
+|  |  | Total time: 1069.00ms <br> Average % over BKS: 9.51% |
--- main.cpp
+++ main.cpp
@@ -5,11 +5,51 @@
 #include <chrono>
 #include <fstream>
 #include <filesystem>
+#include <vector>
+#include <functional>
 #include <unordered_map>
 
 #include "core/cvrp_loader.hpp"
 #include "core/cvrp_solver_savings.hpp"
 #include "core/cvrp_solution_verifier.hpp"
+
+struct InstanceTestResults {
+    float cost;
+    float time_ms;
+};
+
+struct SolverTestResults {
+    std::vector<InstanceTestResults> results;
+    float total_time_ms;
+};
+
+typedef std::function<CVRP::Solution(const CVRP::InstanceData& data)> Solver;
+
+SolverTestResults TestSolver(Solver solver, const std::vector<CVRP::InstanceData>& dataset) {
+    std::vector<InstanceTestResults> results;
+    float total_time_ms = 0;
+
+    for (const auto& instance : dataset) {
+        auto begin = std::chrono::high_resolution_clock::now();
+        auto solution = solver(instance);
+        auto end = std::chrono::high_resolution_clock::now();
+
+        assert(CVRP::Verify(instance, solution));
+
+        float ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+        
+        results.push_back({
+            .cost = solution.total_cost,
+            .time_ms = ms
+        });
+        total_time_ms += ms;
+    }
+
+    return SolverTestResults {
+        .results = std::move(results),
+        .total_time_ms = total_time_ms
+    };
+}
 
 void TestCVRP() {
     if (!std::filesystem::is_directory("test_data/CVRP")) {
@@ -21,7 +61,7 @@ void TestCVRP() {
     for (auto it_dataset : std::filesystem::directory_iterator("test_data/CVRP")) {
         if (it_dataset.is_directory()) {
             std::unordered_map<std::string,float> name_bks;
-            std::unordered_map<std::string,const CVRP::InstanceData> name_instance_data;
+            std::vector<CVRP::InstanceData> dataset;
             
             std::ifstream bks_iff(it_dataset.path() / "BKS.txt");
             while(!bks_iff.eof()) {
@@ -33,49 +73,46 @@ void TestCVRP() {
             auto data_dir = std::filesystem::directory_entry(it_dataset.path() / "data");
             for (auto it_instance : std::filesystem::directory_iterator(data_dir)) {
                 if (it_instance.path().extension() == ".xml") {
-                    auto instance_data = CVRP::LoadVRPREP(it_instance.path());
-                    name_instance_data.insert(std::make_pair(instance_data.name, std::move(instance_data)));
+                    dataset.push_back(CVRP::LoadVRPREP(it_instance.path()));
                 }
             }
 
-            auto dataset_name = name_instance_data.begin()->second.dataset_name;
-            int n_instances = name_instance_data.size();
+            auto dataset_name = dataset.begin()->dataset_name;
+            int n_instances = dataset.size();
+
+            auto savings_results = TestSolver(CVRP::SolveSavings, dataset);
 
             std::cout << std::endl;
             std::cout << "Dataset: " << dataset_name << std::endl; 
 
+            results_out << std::fixed << std::setprecision(2);
             results_out << "## Dataset: " << dataset_name << std::endl;
             results_out << "| Instance | Best known solution | Savings algorithm |" << std::endl;
             results_out << "| --- | --- | --- |" << std::endl;
-            float savings_total_time_ms = 0;
-            float savings_total_bks_ratio = 0;
-            for (const auto& p : name_instance_data) {
-                const auto& name = p.first;
-                const auto& bks = name_bks[name];
 
-                auto begin = std::chrono::high_resolution_clock::now();
-                auto solution = CVRP::SolveSavings(p.second);
-                auto end = std::chrono::high_resolution_clock::now();
+            float savings_total_bks_diff = 0;
+            for (size_t i = 0; i < dataset.size(); ++i) {
+                const auto& name = dataset[i].name;
+                float bks = name_bks[name];
+                const auto& result = savings_results.results[i];
 
-                float ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-                savings_total_time_ms += ms;
+                float cost = result.cost;
+                float time_ms = result.time_ms;
 
-                float cost = solution.total_cost;
-                float bks_ratio = 100 * cost / bks;
-                savings_total_bks_ratio += bks_ratio;
+                float bks_diff = 100 * (cost - bks) / bks;
+                savings_total_bks_diff += bks_diff;
 
-                assert(CVRP::Verify(p.second, solution));
                 std::cout << name << std::endl;
                 std::cout << "  Savings algorithm: " << cost << std::endl;
-                std::cout << "    " << bks_ratio << "% of best known solution" << std::endl;
-                std::cout << "    Time: " << ms << " ms" << std::endl;
+                std::cout << "    " << bks_diff << "% over BKS" << std::endl;
+                std::cout << "    Time: " << time_ms << " ms" << std::endl;
                 std::cout << "  Best known solution: " << bks << std::endl;
 
-                results_out << "| " << name << " | " << bks << " | " << cost << " <br> " << bks_ratio << "% of BKS <br> " << ms << " ms |" << std::endl;  
+                results_out << "| " << name << " | " << bks << " | " << cost << " <br> " << bks_diff << "% over BKS <br> " << time_ms << " ms |" << std::endl;  
             }
-            float savings_average_ratio = savings_total_bks_ratio / n_instances;
-            results_out << "|  |  | Total time: " << savings_total_time_ms << "ms <br> Average ratio: " << savings_average_ratio << "% |" << std::endl;
-            std::cout << "Total savings algorithm time: " << savings_total_time_ms << " ms" << std::endl;
+            float savings_average_diff = savings_total_bks_diff / n_instances;
+            results_out << "|  |  | Total time: " << savings_results.total_time_ms << "ms <br> Average % over BKS: " << savings_average_diff << "% |" << std::endl;
+            std::cout << "Total savings algorithm time: " << savings_results.total_time_ms << " ms" << std::endl;
         }
     }
 }
